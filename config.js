c742a9d85f7eab761fc366ffe66d0c970ba8b6b7
Jared Petersen <jpetersen11@wou.edu>
2015-08-16 19:46:50 -0700

    Travis CI Integration

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     'dbUser': 'api_user',
     'dbPass': 'BKHzZfzjWb8KCsFc4bPBd2HP',
     'dbName': 'projmanage',
-    'apiPort': 8080,
+    'apiPort': 8081,
     'tokenSecret': 'supersecret',
     'tokenExpiration': 1440
 };
